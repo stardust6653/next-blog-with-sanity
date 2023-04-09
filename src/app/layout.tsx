39a552db7079798900b6eb56d42dfb3906b0cba6
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-10 03:23:26 +0900

    Style : header 내 패딩 수정 및 폴더명 변경
    - 폴더 header를 common으로 변경하고 layout 컴포넌트들을 모두 common으로 옮김

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from './components/Header/Footer';
-import Header from './components/Header/Header';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
 
