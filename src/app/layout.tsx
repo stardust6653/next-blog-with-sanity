c3a2e119e7d80c4f49623ee33b67e5a7db3ad521
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-10 02:00:51 +0900

    style : global font 변경
    - Open-Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import { Open_Sans } from 'next/font/google';
+
+const sans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Soye님의 블로그',
@@ -7,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>{children}</body>
     </html>
   );
