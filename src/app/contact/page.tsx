c0f4389a4f1701427bec47ff4e42a5b5acea5cc7
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-10 14:39:08 +0900

    feat: contact 페이지 마크업 및 스타일링

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Contact from '../components/contactView/contact';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="flex justify-center">
+      <Contact />
+    </main>
+  );
 };
 
 export default page;
