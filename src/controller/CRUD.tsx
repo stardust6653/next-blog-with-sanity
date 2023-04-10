7e239cae5c094e522be1fd4163087bf93ba44bed
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-10 22:01:51 +0900

    feat : MVC 패턴을 위해 로직 분리
    - CRUD폴더 생성해 CRUD함수 분리

---
+++ src/controller/CRUD.tsx
@@ -0,0 +1,35 @@
+import { db } from '@/firebase';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+
+interface DataProps {
+  title: string;
+  md: string;
+}
+
+export const getData = async () => {
+  const dataArr: DataProps[] = [];
+
+  const postRef = collection(db, 'posts');
+  const q = query(postRef);
+  const querySnapShot = await getDocs(q);
+
+  querySnapShot.forEach((item) => {
+    const returnDoc = item.data() as DataProps;
+    dataArr.push(returnDoc);
+  });
+
+  return dataArr;
+};
+
+interface PostProps {
+  id: number;
+  title: string;
+  md: string;
+}
+
+export const postData = (event: React.FormEvent<HTMLFormElement>, data: PostProps) => {
+  event.preventDefault();
+  const postRef = collection(db, 'posts');
+
+  addDoc(postRef, data);
+};
