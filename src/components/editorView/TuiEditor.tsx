b19a6466d1c95ffb88faf546b72520b2802bb092
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-11 21:35:47 +0900

    feat : 추가 정보 입력창 추가

--- src/components/editorView/TuiEditor.tsx
+++ src/components/editorView/TuiEditor.tsx
@@ -6,13 +6,17 @@ import { Editor } from '@toast-ui/react-editor';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import codeSyntaxHighlightPlugin from '@toast-ui/editor-plugin-code-syntax-highlight';
 import React, { useEffect, useRef, useState } from 'react';
-import { postData } from '@/controller/CRUD';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import MoreInfo from './MoreInfo';
 
 const TuiEditor = () => {
   const viewHeight = Number(window.outerHeight - 450) + 'px';
   const editorRef = useRef<any>(null);
+  const router = useRouter();
   const [title, setTitle] = useState('');
-  const [markdown, setMarkdown] = useState('');
+  const [html, setHTML] = useState('');
+  const [visiable, setVisible] = useState(false);
 
   const toolbarItems = [
     ['heading', 'bold', 'italic', 'strike'],
@@ -24,58 +28,61 @@ const TuiEditor = () => {
     ['scrollSync'],
   ];
 
-  const data = {
-    id: Date.now(),
-    title,
-    markdown,
-  };
-
   useEffect(() => {
-    setMarkdown(() => editorRef.current.getInstance().getMarkdown());
-  }, [markdown]);
+    setHTML(() => editorRef.current.getInstance().getHTML());
+  }, [html]);
 
   return (
-    <form
-      className="w-3/5"
-      onSubmit={(event) => {
-        event.preventDefault();
-        postData(event, data);
-      }}
-    >
-      <input
-        placeholder="제목을 입력해주세요."
-        className="text-4xl mb-5 outline-none pl-5 pr-5 w-full"
-        onChange={(event) => {
-          setTitle(event.target.value);
-        }}
-        required
-      />
-      <Editor
-        onChange={() => {
-          setMarkdown(editorRef.current.getInstance().getMarkdown());
-        }}
-        initialValue="글을 적어주세요!"
-        previewStyle="vertical"
-        height={viewHeight}
-        initialEditType="markdown"
-        useCommandShortcut={false}
-        plugins={[colorSyntax, codeSyntaxHighlightPlugin]}
-        language="ko-KR"
-        toolbarItems={toolbarItems}
-        ref={editorRef}
-      />
-      <div className="flex justify-end mr-3">
-        <button className="p-5 pt-3 pb-3 mt-5 mr-2 rounded-lg text-amber-50 font-semibold transition ease-in-out delay-150 bg-rose-500  hover:bg-red-600 duration-300">
-          취소하기
-        </button>
-        <button
-          type="submit"
-          className="p-5 pt-3 pb-3 mt-5 rounded-lg text-amber-50 font-semibold transition ease-in-out delay-150 bg-blue-500  hover:bg-indigo-500 duration-300"
+    <>
+      {visiable ? (
+        <MoreInfo title={title} html={html} />
+      ) : (
+        <form
+          className="w-3/5"
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
         >
-          출간하기
-        </button>
-      </div>
-    </form>
+          <input
+            placeholder="제목을 입력해주세요."
+            className="text-4xl mb-5 outline-none pl-5 pr-5 w-full"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            required
+          />
+          <Editor
+            onChange={() => {
+              setHTML(editorRef.current.getInstance().getHTML());
+              console.log(html);
+            }}
+            initialValue="글을 적어주세요!"
+            previewStyle="vertical"
+            height={viewHeight}
+            initialEditType="markdown"
+            useCommandShortcut={false}
+            plugins={[colorSyntax, codeSyntaxHighlightPlugin]}
+            language="ko-KR"
+            toolbarItems={toolbarItems}
+            ref={editorRef}
+          />
+          <div className="flex justify-end mr-3">
+            <button className="p-5 pt-3 pb-3 mt-5 mr-2 rounded-lg text-amber-50 font-semibold transition ease-in-out delay-150 bg-rose-500  hover:bg-red-600 duration-300">
+              <Link href="/posts">취소하기</Link>
+            </button>
+            <button
+              type="submit"
+              className="p-5 pt-3 pb-3 mt-5 rounded-lg text-amber-50 font-semibold transition ease-in-out delay-150 bg-blue-500  hover:bg-indigo-500 duration-300"
+              onClick={() => {
+                setVisible(true);
+              }}
+            >
+              출간하기
+            </button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
