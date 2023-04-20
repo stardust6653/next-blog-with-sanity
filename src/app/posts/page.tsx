9653d7bfa054658a297d91e205c2a8c455346eb6
Soye Park <stardust6653@gmail.com>
2023-04-20 18:04:30 +0900

    Style : 반응형 일부 적용
    - Header
    - Post
    - Home

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -15,7 +15,7 @@ const page = async () => {
     <section className="flex justify-center mt-8 w-full mb-10">
       <div className="flex flex-col items-center">
         <h2 className="text-4xl font-bold">Post</h2>
-        <div className="md:w-12/12 lg:w-8/12 flex flex-wrap justify-center">
+        <div className="md:w-12/12 lg:w-9/12 flex flex-wrap justify-center">
           {data.map((item, i) => {
             return (
               <PostCard
