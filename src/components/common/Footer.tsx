52bca565b942f14c04fd6d4af942ea5940f9ce13
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-20 15:42:42 +0900

    Refactor : 하드코딩 되어 있던 저작권 연도를 Date 메소드로 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-300 p-6 flex justify-center">
-      Copyright&copy; 2023.soyepark.All rights reserved.
+      Copyright&copy; {new Date().getFullYear()}.soyepark.All rights reserved.
     </footer>
   );
 };
