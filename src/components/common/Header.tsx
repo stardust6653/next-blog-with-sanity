d5a4766cbccbbf90b47efa0cab6e626a59762fb2
Soye Park <stardust6653@gmail.com>
2023-04-20 15:55:42 +0900

    feat : 로고에 home으로 Link 추가 및 CSS작업

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,11 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-gray-800 flex justify-around items-center pl-16 pr-16 pt-5 pb-5 pr">
-      <h1 className="text-gray-300 text-2xl">Soyeah Blog</h1>
+      <h1 className="text-gray-300 text-2xl font-bold">
+        <Link href="/">
+          <span className=" text-yellow-500">Soyeah</span> Blog
+        </Link>
+      </h1>
       <nav>
         <ul className="flex">
           <li className="text-gray-300 ml-8">
