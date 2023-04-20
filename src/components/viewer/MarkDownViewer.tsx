cb8a1b0c24e74a534b458befb66cbbe488f46089
박소예 <soyepark@bagsoyes-MacBook-Pro.local>
2023-04-20 15:43:52 +0900

    style : Viewer CSS font-size와 기타 여백, img 사이즈 수정

--- src/components/viewer/MarkDownViewer.tsx
+++ src/components/viewer/MarkDownViewer.tsx
@@ -4,13 +4,18 @@ import React from 'react';
 
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import style from './MarkDownViewer.module.css';
 
 interface ValueProps {
   html: string;
 }
 
 const MarkDownViewer = ({ html }: ValueProps) => {
-  return <Viewer initialValue={html} />;
+  return (
+    <div className={style.container}>
+      <Viewer initialValue={html} />
+    </div>
+  );
 };
 
 export default MarkDownViewer;
