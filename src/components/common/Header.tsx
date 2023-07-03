17a725d53129bb360791034880b943c8ce333017
Soye Park <stardust6653@gmail.com>
2023-07-03 15:15:30 +0900

    refactor : menu 목록 배열로 분리 및 map 으로 반복

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
 
+type Props = {
+  name: string;
+  href: string;
+};
+
+const menu: Props[] = [
+  { name: 'Home', href: '/' },
+  { name: 'Posts', href: '/posts' },
+  { name: 'Login', href: '#' },
+];
+
 const Header = () => {
   return (
     <header className="bg-gray-800 flex justify-around items-center pl-2 pr-2 pt-5 pb-5">
@@ -11,15 +22,13 @@ const Header = () => {
       </h1>
       <nav>
         <ul className="flex ">
-          <li className="text-gray-300">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="text-gray-300 ml-8">
-            <Link href="/posts">Posts</Link>
-          </li>
-          {/* <li className="text-gray-300 ml-8">
-            <Link href="/admin">Admin</Link>
-          </li> */}
+          {menu.map((item) => {
+            return (
+              <li key={item.name} className="text-gray-300 ml-8">
+                <Link href={item.href}>{item.name}</Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
