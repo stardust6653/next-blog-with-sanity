35d4c8c044a1f9c1d022b33081d15c07e5fbd819
Soye Park <stardust6653@gmail.com>
2023-07-04 14:39:22 +0900

    feat : 카카오, 깃헙 로그인 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import KakaoProvider from 'next-auth/providers/kakao';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -7,7 +9,18 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_SECRET,
+    }),
   ],
+  pages: {
+    signIn: '/auth/signin',
+  },
 };
 const handler: NextAuthOptions = NextAuth(authOptions);
 
