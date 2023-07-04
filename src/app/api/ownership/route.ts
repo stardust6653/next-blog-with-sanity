92d03e49b42317df6185850202e1d2cf361f35f3
Soye Park <stardust6653@gmail.com>
2023-07-04 20:47:24 +0900

    feat : DB에서 관리자 계정 여부 확인 가능하도록 하는 백엔드 로직 작성

---
+++ src/app/api/ownership/route.ts
@@ -0,0 +1,15 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
+import { getOwnership } from '@/service/user';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  return getOwnership(user.id).then((data) => NextResponse.json(data));
+}
