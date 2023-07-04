65eefa0f30582527b4121d564cb6e4a96f072fc6
Soye Park <stardust6653@gmail.com>
2023-07-04 13:16:59 +0900

    feat : 로그인 유무에 따른 메뉴 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Button from '../ui/Button';
 
 type Props = {
   name: string;
@@ -9,10 +13,11 @@ type Props = {
 const menu: Props[] = [
   { name: 'Home', href: '/' },
   { name: 'Posts', href: '/posts' },
-  { name: 'Login', href: '#' },
 ];
 
 const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <header className="bg-gray-800 flex justify-around items-center pl-2 pr-2 pt-5 pb-5">
       <h1 className="text-gray-300 md:text-xl lg:text-2xl font-bold">
@@ -24,11 +29,21 @@ const Header = () => {
         <ul className="flex ">
           {menu.map((item) => {
             return (
-              <li key={item.name} className="text-gray-300 ml-8">
+              <li key={item.name} className="text-gray-300 ml-8 hover:text-yellow-500">
                 <Link href={item.href}>{item.name}</Link>
               </li>
             );
           })}
+          {session ? (
+            <>
+              <li key="Create" className="text-gray-300 ml-8 hover:text-yellow-500">
+                <Link href="/create">Create</Link>
+              </li>
+              <Button text="Log Out" onClick={() => signOut()} />
+            </>
+          ) : (
+            <Button text="Log In" onClick={() => signIn()} />
+          )}
         </ul>
       </nav>
     </header>
