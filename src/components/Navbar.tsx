287c822c6128dfc9c9aa72d146b2a0eef0574bc5
Soye Park <stardust6653@gmail.com>
2023-07-04 17:19:37 +0900

    refactor : Navbar 별도로 분리

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Button from './ui/Button';
+import Avatar from './Avatar';
+
+type Props = {
+  name: string;
+  href: string;
+};
+
+const menu: Props[] = [
+  { name: 'Home', href: '/' },
+  { name: 'Posts', href: '/posts' },
+];
+
+const Navbar = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  return (
+    <nav>
+      <ul className="flex text-base">
+        {menu.map((item) => {
+          return (
+            <li key={item.name} className="text-gray-300 ml-8 hover:text-yellow-500">
+              <Link href={item.href}>{item.name}</Link>
+            </li>
+          );
+        })}
+
+        {session ? (
+          <>
+            <li key="Create" className="text-gray-300 ml-8 hover:text-yellow-500">
+              <Link href="/create">Create</Link>
+            </li>
+            <li>
+              <Button text="Log Out" onClick={() => signOut()} />
+            </li>
+          </>
+        ) : (
+          <li>
+            <Button text="Log In" onClick={() => signIn()} />
+          </li>
+        )}
+        {user && (
+          <li>
+            <Link href={`/user/${user.name}`}>
+              <Avatar image={user.image} />
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
