b9fcb8bd8c24bdca8ee84fde32d73c5c1b5e737d
Soye Park <stardust6653@gmail.com>
2023-07-04 17:20:10 +0900

    refactor : Navbar 별도로 분리 및 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,8 +2,7 @@
 
 import Link from 'next/link';
 import React from 'react';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import Button from '../ui/Button';
+import Navbar from '../Navbar';
 
 type Props = {
   name: string;
@@ -16,36 +15,14 @@ const menu: Props[] = [
 ];
 
 const Header = () => {
-  const { data: session } = useSession();
-
   return (
     <header className="bg-gray-800 flex justify-around items-center pl-2 pr-2 pt-5 pb-5">
       <h1 className="text-gray-300 md:text-xl lg:text-2xl font-bold">
         <Link href="/">
-          <span className=" text-yellow-500">Soyeah</span> Blog
+          <span className="text-yellow-500">Soyeah</span> Blog
         </Link>
       </h1>
-      <nav>
-        <ul className="flex ">
-          {menu.map((item) => {
-            return (
-              <li key={item.name} className="text-gray-300 ml-8 hover:text-yellow-500">
-                <Link href={item.href}>{item.name}</Link>
-              </li>
-            );
-          })}
-          {session ? (
-            <>
-              <li key="Create" className="text-gray-300 ml-8 hover:text-yellow-500">
-                <Link href="/create">Create</Link>
-              </li>
-              <Button text="Log Out" onClick={() => signOut()} />
-            </>
-          ) : (
-            <Button text="Log In" onClick={() => signIn()} />
-          )}
-        </ul>
-      </nav>
+      <Navbar />
     </header>
   );
 };
