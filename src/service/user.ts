c2be008b39174f0725ffa488b767383c70ec505b
Soye Park <stardust6653@gmail.com>
2023-07-04 20:46:46 +0900

    feat : 아이디 생성 시 id추가 및 id를 토대로 관리자 계정인 지 파악할 수 있도록 함

--- src/service/user.ts
+++ src/service/user.ts
@@ -12,10 +12,19 @@ export const addUser = async ({ id, username, name, email, image }: OAuthUser) =
   return client.createIfNotExists({
     _id: id,
     _type: 'user',
+    id,
     username,
     email,
     image,
     name,
     bookmarks: [],
   });
+};
+
+export const getOwnership = async (id: string | undefined) => {
+  return client.fetch(
+    `*[_type == "user" && id == "${id}"]{
+      "owner": owner
+    }`
+  );
 };
