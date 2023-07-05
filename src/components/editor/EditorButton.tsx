1da59266b6bff69c08eb87764a25122a768236d3
Soye Park <stardust6653@gmail.com>
2023-07-06 01:42:42 +0900

    feat : setVisible Props 제거  및 onClick Props 사용

--- src/components/editor/EditorButton.tsx
+++ src/components/editor/EditorButton.tsx
@@ -7,16 +7,15 @@ type Props = {
   color: string;
   type?: 'button' | 'submit' | 'reset' | undefined;
   onClick?: () => void;
-  setVisible?: any;
   href?: string;
 };
 
-const EditorButton = ({ text, color, onClick, setVisible, type = 'button', href }: Props) => {
+const EditorButton = ({ text, color, onClick, type = 'button', href }: Props) => {
   return (
     <button
       type={type}
       className={`${color} ml-2 p-5 pt-3 pb-3 mt-5 rounded-lg text-amber-50 font-semibold transition ease-in-out delay-150 duration-300`}
-      onClick={() => setVisible(true)}
+      onClick={onClick}
     >
       {href ? <Link href={href}>{text}</Link> : <>{text}</>}
     </button>
