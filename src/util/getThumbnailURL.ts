3b7b7ba0326031c85b712c947a686072ce25c512
Soye Park <stardust6653@gmail.com>
2023-07-06 00:43:03 +0900

    refactor : imageURL을 얻는 함수를 util 함수로 분리

---
+++ src/util/getThumbnailURL.ts
@@ -0,0 +1,12 @@
+export const getThumbnailURL = async (blob: File, callback: (url: string) => void) => {
+  if (!blob) {
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('photo', blob);
+
+  await fetch('/api/posts/', { method: 'POST', body: formData })
+    .then((res) => res.json())
+    .then((result) => callback(result.url));
+};
