79e434b7dfa6e4b7b95445eb6e1bd36538884027
Soye Park <stardust6653@gmail.com>
2023-07-05 23:12:18 +0900

    chore : Firebase => Sanity 이전으로 파이어베이스 관련 코드 임시 주석 처리

--- src/controller/CRUD.tsx
+++ src/controller/CRUD.tsx
@@ -1,53 +1,53 @@
-import { addDoc, collection, getDocs, orderBy, query } from 'firebase/firestore';
-import { db } from '../firebase';
+// import { addDoc, collection, getDocs, orderBy, query } from 'firebase/firestore';
+// import { db } from '../firebase';
 
-interface DataProps {
-  id: number;
-  title: string;
-  html: string;
-  date: string;
-  previewImg: string;
-  description: string;
-}
+// interface DataProps {
+//   id: number;
+//   title: string;
+//   html: string;
+//   date: string;
+//   previewImg: string;
+//   description: string;
+// }
 
-export const getData = async () => {
-  const dataArr: DataProps[] = [];
+// export const getData = async () => {
+//   const dataArr: DataProps[] = [];
 
-  const postRef = collection(db, 'posts');
-  const q = query(postRef);
-  const querySnapShot = await getDocs(q);
+//   const postRef = collection(db, 'posts');
+//   const q = query(postRef);
+//   const querySnapShot = await getDocs(q);
 
-  querySnapShot.forEach((item) => {
-    const returnDoc = item.data() as DataProps;
-    dataArr.push(returnDoc);
-  });
+//   querySnapShot.forEach((item) => {
+//     const returnDoc = item.data() as DataProps;
+//     dataArr.push(returnDoc);
+//   });
 
-  return dataArr;
-};
+//   return dataArr;
+// };
 
-interface PostProps {
-  id: number;
-  title: string;
-  html: string;
-  date: string;
-}
+// interface PostProps {
+//   id: number;
+//   title: string;
+//   html: string;
+//   date: string;
+// }
 
-export const postData = (data: PostProps) => {
-  const postRef = collection(db, 'posts');
+// export const postData = (data: PostProps) => {
+//   const postRef = collection(db, 'posts');
 
-  addDoc(postRef, data);
-};
+//   addDoc(postRef, data);
+// };
 
-export const getDescFilteredData = async () => {
-  const postRef = collection(db, 'posts');
-  const queryData = query(postRef, orderBy('id', 'desc'));
-  const querySnapShot = await getDocs(queryData);
-  const descFilteredDataArr: DataProps[] = [];
+// export const getDescFilteredData = async () => {
+//   const postRef = collection(db, 'posts');
+//   const queryData = query(postRef, orderBy('id', 'desc'));
+//   const querySnapShot = await getDocs(queryData);
+//   const descFilteredDataArr: DataProps[] = [];
 
-  querySnapShot.forEach((item) => {
-    const returnDoc = item.data() as DataProps;
-    descFilteredDataArr.push(returnDoc);
-  });
+//   querySnapShot.forEach((item) => {
+//     const returnDoc = item.data() as DataProps;
+//     descFilteredDataArr.push(returnDoc);
+//   });
 
-  return descFilteredDataArr;
-};
+//   return descFilteredDataArr;
+// };
