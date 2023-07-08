f27d6462eb0d0bf8978dc3e8d1a6fc32bd9eade4
Soye Park <stardust6653@gmail.com>
2023-07-08 13:03:24 +0900

    feat : POST 추가
    - 포스트 작성을 위한 POST 추가

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,25 @@
-import { createImageURL } from '@/service/posts';
+import { createPost, getPost } from '../../../service/posts';
 import { NextRequest, NextResponse } from 'next/server';
+import { DataProps } from '../../../../types/data';
 
 export async function POST(req: NextRequest) {
   const form = await req.formData();
-  const photo = form.get('photo') as Blob;
 
-  return createImageURL(photo).then((data) => NextResponse.json(data));
+  const title = form.get('title')?.toString() ?? '';
+  const content = form.get('content')?.toString() ?? '';
+  const imgUrl = form.get('imgUrl')?.toString() ?? '';
+  const description = form.get('description')?.toString() ?? '';
+
+  const dataObj: DataProps = {
+    title,
+    content,
+    imgUrl,
+    description,
+  };
+
+  if (!title || !content || !imgUrl || !description) {
+    return new Response('Bad Request', { status: 400 });
+  }
+
+  return createPost(dataObj).then((data) => NextResponse.json(data));
 }
