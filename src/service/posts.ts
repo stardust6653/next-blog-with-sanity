f33649c3030e95ea40c02b7c37180d27a83baaed
Soye Park <stardust6653@gmail.com>
2023-07-08 13:07:13 +0900

    feat : 포스트 데이터 GET 로직 추가

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,3 +1,5 @@
+import { title } from 'process';
+import { DataProps } from '../../types/data';
 import { assetsURL, client } from './sanity';
 
 export async function createImageURL(photo: Blob) {
@@ -21,4 +23,50 @@ export async function createImageURL(photo: Blob) {
         { autoGenerateArrayKeys: true }
       );
     });
+}
+
+export async function createPost(dataObj: DataProps) {
+  const { title, content, imgUrl, description } = dataObj;
+  return client.create(
+    {
+      _type: 'post',
+      title,
+      content,
+      imgUrl,
+      description,
+      likes: [],
+      comments: [],
+    },
+    { autoGenerateArrayKeys: true }
+  );
+}
+
+export async function getPost() {
+  return client.fetch(
+    `*[_type == "post"] | order(_createdAt desc){
+      "title": title,
+      "content": content,
+      "likes": likes[] -> username,
+      "thumbnail" : imgUrl,
+      "comments": count(comments),
+      "id": _id,
+      "createdAt": _createdAt,
+      "description": description
+    }`
+  );
+}
+
+export async function getNewPosts() {
+  return client.fetch(
+    `*[_type == "post"] | order(_createdAt desc) [0...8]{
+      "title": title,
+      "content": content,
+      "likes": likes[] -> username,
+      "thumbnail" : imgUrl,
+      "comments": count(comments),
+      "id": _id,
+      "createdAt": _createdAt,
+      "description": description
+    }`
+  );
 }
