2065bc91c2705ab7d0e9d09be43af1a67360c820
Soye Park <stardust6653@gmail.com>
2023-07-08 13:04:22 +0900

    chore : 절대경로 오류로 import구문 상대경로로 임시로 수정

--- src/app/api/ownership/route.ts
+++ src/app/api/ownership/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]/route';
-import { getOwnership } from '@/service/user';
+import { getOwnership } from '../../../service/user';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
