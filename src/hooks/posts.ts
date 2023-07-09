d236c79ea9fdc83100888d3d2cffe979472586ff
Soye Park <stardust6653@gmail.com>
2023-07-09 16:43:54 +0900

    feat : 데이터 패칭 및 mutate 처리를 위한 customHook 추가

---
+++ src/hooks/posts.ts
@@ -0,0 +1,46 @@
+'use client';
+
+import { CardProps } from '@/components/PostCard';
+import useSWR, { useSWRConfig } from 'swr';
+
+type SimplePostProps = {
+  comments: number;
+  content: string;
+  createdAt: string;
+  description: string;
+  id: string;
+  likes: string[];
+  thumbnail: string;
+  title: string;
+  viewCount: number;
+};
+
+async function updateLike(id: string, like: boolean) {
+  fetch('/api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
+
+export default function usePosts() {
+  const { data: posts, isLoading, error, mutate } = useSWR<any>(`/api/posts`);
+
+  const setLike = (post: SimplePostProps, username: string, like: boolean) => {
+    const likes = post.likes ?? [];
+
+    const newPost = {
+      ...post,
+      likes: like ? [...likes, username] : likes.filter((item: string) => item !== username),
+    };
+    const newPosts = posts?.map((p: CardProps) => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
+  };
+
+  return { posts, isLoading, error, setLike };
+}
