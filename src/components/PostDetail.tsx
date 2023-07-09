b87bd68b14dab3fd09ae755597a0aa2052eead64
Soye Park <stardust6653@gmail.com>
2023-07-09 16:44:59 +0900

    feat : 조건문 작성해 undefined 대응

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,45 +1,62 @@
 'use client';
 
-import React, { useEffect } from 'react';
-import useSWR from 'swr';
-import MarkDownViewer from './viewer/MarkDownViewer';
+import React from 'react';
 import Image from 'next/image';
 import { parseDate } from '@/util/date';
+import SideBar from './SideBar';
+import dynamic from 'next/dynamic';
+import usePosts from '@/hooks/posts';
+import { CardProps } from './PostCard';
 import { viewCountUpdate } from '@/util/viewCountUpdate';
 
+const MarkDownViewer = dynamic(() => import('./viewer/MarkDownViewer'), {
+  ssr: false,
+});
+
 type Props = {
-  slug: string;
+  params: {
+    slug: string;
+  };
 };
 
-const PostDetail = ({ params }: any) => {
+const PostDetail = ({ params }: Props): any => {
   const id = params.slug;
-  const { data: post, isLoading: loading, error } = useSWR(`/api/posts/${id}`);
+  const { posts, isLoading: loading } = usePosts();
 
-  useEffect(() => {
-    post && viewCountUpdate(post[0].id, post[0].viewCount);
-  }, [post]);
+  if (posts !== undefined) {
+    const arrayPosts: CardProps[] = Array.from(posts);
 
-  return (
-    <>
-      {post && (
-        <article className="sm: w-11/12 m-5 xl:w-7/12 mt-12 mb-12">
-          <h1 className="sm: text-3xl xl:text-5xl font-bold mb-6">{post[0].title}</h1>
-          <p className="mb-4 text-end">발행일 : {parseDate(post[0].createdAt)}</p>
-          <Image
-            src={post[0].thumbnail}
-            alt=""
-            width="320"
-            height="208"
-            placeholder="blur"
-            blurDataURL={post[0].thumbnail}
-            style={{ objectFit: 'cover' }}
-            className="shadow-md rounded-xl overflow-hidden bg-no-repeat h-52 bg-center"
-          />
-          <MarkDownViewer html={post[0].content} />
-        </article>
-      )}
-    </>
-  );
+    const post = () => {
+      return arrayPosts.filter((item: CardProps) => item.id === id)[0];
+    };
+
+    viewCountUpdate(post().id, post().viewCount);
+
+    return (
+      <>
+        {loading && <p>로딩 중입니당</p>}
+
+        {post() && (
+          <article className="sm: w-11/12 m-5 xl:w-7/12 mt-12 mb-12 relative">
+            <SideBar post={post()} />
+            <div>
+              <h1 className="sm: text-3xl xl:text-5xl font-bold mb-6">{post()?.title}</h1>
+              <p className="mb-4 text-end">{parseDate(post()?.createdAt)}</p>
+              <Image
+                src={post()?.thumbnail}
+                alt=""
+                width="320"
+                height="208"
+                style={{ objectFit: 'cover' }}
+                className="shadow-md rounded-xl overflow-hidden bg-no-repeat h-52 bg-center"
+              />
+              <MarkDownViewer html={post()?.content} />
+            </div>
+          </article>
+        )}
+      </>
+    );
+  }
 };
 
 export default PostDetail;
