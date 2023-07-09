2437579c4ca797d708a15bddfb1b1547be29ffc0
Soye Park <stardust6653@gmail.com>
2023-07-09 18:22:43 +0900

    feat : 하트아이콘에 size props 추가

--- src/components/ui/icons/HeartIcon.tsx
+++ src/components/ui/icons/HeartIcon.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { AiOutlineHeart } from 'react-icons/ai';
 
-const HeartIcon = () => {
-  return <AiOutlineHeart className={`text-4xl  text-gray-700`} />;
+const HeartIcon = ({ size }: { size?: string }) => {
+  return <AiOutlineHeart className={`text-4xl ${size} text-gray-700`} />;
 };
 
 export default HeartIcon;
