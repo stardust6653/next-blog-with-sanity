23433c5be85978b7117fe9bb41ff3ea3793118e2
Soye Park <stardust6653@gmail.com>
2023-07-10 14:53:08 +0900

    feat : 북마크 리스트 추가

---
+++ src/components/BookmarkList.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+import useSWR from 'swr';
+import PostCard from './PostCard';
+import usePosts from '../hooks/posts';
+
+interface CardProps {
+  thumbnail?: string;
+  comments?: number;
+  content?: string;
+  createdAt?: string;
+  id?: string;
+  likes?: string[];
+  title?: string;
+  description?: string;
+}
+
+const BookmarkList = () => {
+  const { data: posts, isLoading: loading } = useSWR('/api/bookmarksPost');
+
+  return (
+    <>
+      {loading && <p>로딩중입니당</p>}
+
+      {posts && (
+        <ul className="grid grid-cols-4 w-full">
+          {posts?.map((post: CardProps, index: number) => (
+            <li key={post.id}>
+              <PostCard key={index} post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+export default BookmarkList;
