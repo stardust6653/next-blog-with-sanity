8071d7b3b77e673a922ffab3fc3007029ef2f47d
Soye Park <stardust6653@gmail.com>
2023-07-10 18:59:40 +0900

    feat : 로더 컴포넌트 및 원하는 포스트 없음 컴포넌트 제작

---
+++ src/components/DoNotFindPost.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DoNotFindPost = () => {
+  return (
+    <div className=" flex justify-center items-center h-96">
+      <p className="text-lg font-base">찾으시는 포스트가 없어요. 다시 한번 확인해주세요!</p>
+    </div>
+  );
+};
+
+export default DoNotFindPost;
