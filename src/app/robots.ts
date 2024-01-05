aa579282f773a67510f4d78e9fdd3563abbc4efe
stardust6653 <stardust6653@gmail.com>
2024-01-05 12:00:07 +0900

    feat : robots.txt 파일 제거 및 robots.ts 파일로 동적 생성

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next';
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.soyeah-blog.xyz//sitemap.xml',
+    host: 'https://www.soyeah-blog.xyz/',
+  };
+}
