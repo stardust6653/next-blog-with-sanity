731bd437d73e23ae235ac828d33f54da8972583e
stardust6653 <stardust6653@gmail.com>
2024-01-05 11:44:09 +0900

    refactor : 사용하지 않는 임포트 제거

--- src/components/main/Introduction/index.tsx
+++ src/components/main/Introduction/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 import styles from './Introduction.module.scss';
-import Title from './components/Title';
 import IntroImage from './components/IntroImage';
 import MyInfo from './components/MyInfo';
-import SubTitle from './components/SubTitle';
-import Description from './components/Description';
 import TitleBox from './components/TitleBox';
 
 const Introduction = () => {
