6840bf00f56ebd3255b3b83c92f999b6949b622e
stardust6653 <stardust6653@gmail.com>
2024-01-19 18:25:14 +0900

    feat : 사이트맵 정적 생성
    - 왜 동적 생성이 계속 안되는 것이냐...ㅠ

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,6 @@
 import { MetadataRoute } from 'next';
-import { Card } from '../../types/types';
-
-export const getPosts = async () => {
-  return fetch('/api/posts', { next: { revalidate: 60 * 30 }, method: 'GET' })
-    .then((res) => {
-      if (!res.ok) {
-        return Promise.reject();
-      }
-      return res.json();
-    })
-    .catch(() => {
-      return [];
-    });
-};
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-  const posts: Card[] = await getPosts();
-
-  const blogPosts = posts.map((post) => ({
-    url: `https://www.soyeah-blog.xyz/posts/${post.id}`,
-    lastModified: post.createdAt,
-  }));
-
-  console.log(blogPosts);
-
   return [
     {
       url: 'https://www.soyeah-blog.xyz',
@@ -33,7 +10,178 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
       url: 'https://www.soyeah-blog.xyz/posts',
       lastModified: new Date(),
     },
-    ...blogPosts,
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdyqwV1',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdyaaQl',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOnbrEs',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdyrbkR',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdysIA7',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOo4Fin',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOo5lJm',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfgJcgW',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOidON6',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdvKuPj',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfgdUcY',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOiqN1K',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOitpbM',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOiyNfu',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOmbLKQ',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOmeZj6',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QflaYlk',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QflhmZL',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfltlKA',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOn6Hex',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfmOtMr',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdyRWtH',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdyTgAV',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOnTBzW',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfnObhF',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdz0ea3',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOoB0yp',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfnUv1e',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/nlZ529yuBuiTdrNiOoE7Ql',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfnXgSc',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/jS823klo7kYKG05QfnYVKa',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Q2GfoZ2timmiQVzmdz5ixf',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/31SgN7qkpPCqZsKHAC3gUJ',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/bdQOzix2CF8DeXgUm6qmSB',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/btCCAzAIGueZmYG3oODCKo',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/p8llk913miEieLJga4EZol',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/AIRHb0MznO03XM9vOYsY0b',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Fovhr43E33CsodAANTcfwe',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Fovhr43E33CsodAANTeN0a',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/Fovhr43E33CsodAANTf2Wm',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/mA9gCQ33Tz3TyAYoADukVm',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/2zOHGqgcWHHP7ISVK2B2XU',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.soyeah-blog.xyz/posts/r8QI4XsJMHUG6h19GVdiB3',
+      lastModified: new Date(),
+    },
   ];
 };
 
